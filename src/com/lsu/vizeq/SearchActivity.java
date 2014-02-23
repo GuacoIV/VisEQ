c48b0e8bd03bdd581e70cc1226771baa9e4f603d
GuacoIV <edward.lynch@att.net>
2014-02-23 14:44:06 -0600

    Search Looks a bit better

--- src/com/lsu/vizeq/SearchActivity.java
+++ src/com/lsu/vizeq/SearchActivity.java
@@ -9,12 +9,17 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -23,6 +28,8 @@ import android.widget.TextView;
 
 public class SearchActivity extends Activity
 {
+	LinearLayout searchLayout;
+
 	AsyncHttpClient searchClient = new AsyncHttpClient();
 	WebService web = new WebService("");
 	@Override
@@ -30,9 +37,38 @@ public class SearchActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
-		final LinearLayout searchLayout = (LinearLayout) findViewById(R.id.SearchLayout);
+		searchLayout = (LinearLayout) findViewById(R.id.SearchLayout);
 		final EditText searchText = (EditText) findViewById(R.id.SearchField);
+		final OnTouchListener rowTap;
 		
+		rowTap = new OnTouchListener()
+		{
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1)
+			{
+				if (arg1.getAction() == MotionEvent.ACTION_DOWN)
+				{
+					TableRow row = (TableRow)arg0;
+					row.setBackgroundColor(Color.BLUE);
+					return true;
+				}
+				if (arg1.getAction() == MotionEvent.ACTION_UP)
+				{					
+					TrackRow row = (TrackRow)arg0;
+					row.setBackgroundColor(row.originalColor);
+					return true;
+				}
+				if (arg1.getAction() == MotionEvent.ACTION_CANCEL)
+				{
+					TrackRow row = (TrackRow)arg0;
+					row.setBackgroundColor(row.originalColor);
+					return true;
+				}
+				return false;
+			}
+		
+		};
 		
 		
 		findViewById(R.id.SearchOK).setOnClickListener(new OnClickListener(){
@@ -41,6 +77,7 @@ public class SearchActivity extends Activity
 			public void onClick(View arg0)
 			{
 				// TODO Auto-generated method stub
+				searchLayout.removeAllViews();
 				String strSearch = searchText.getText().toString();
 				strSearch = strSearch.replace(' ', '+');
 				searchClient.get("http://ws.spotify.com/search/1/track.json?q=" + strSearch, new JsonHttpResponseHandler() {
@@ -52,12 +89,35 @@ public class SearchActivity extends Activity
 							for (int i = 0; i < tracks.length(); i++)
 							{
 								String trackName = tracks.getJSONObject(i).getString("name");
+								String trackArtist = tracks.getJSONObject(i).getJSONArray("artists").getJSONObject(0).getString("name");
 								String uri = tracks.getJSONObject(i).getString("href");
-								Log.d("Search", trackName + ": " + uri);
-								TableRow tableRowToAdd = new TableRow(SearchActivity.this);
+								//Log.d("Search", trackName + ": " + trackArtist);
+								TrackRow tableRowToAdd = new TrackRow(SearchActivity.this);
 								TextView textViewToAdd = new TextView(SearchActivity.this);
+								TextView textTwoViewToAdd = new TextView(SearchActivity.this);
+								tableRowToAdd.mTrack = trackName;
+								tableRowToAdd.mArtist = trackArtist;
+								tableRowToAdd.mUri = uri;
+								if (i % 2 == 0) 
+								{
+									tableRowToAdd.setBackgroundColor(Color.rgb(200, 200, 200));
+									tableRowToAdd.originalColor = Color.rgb(200, 200, 200);
+								}
+								else
+								{
+									tableRowToAdd.setBackgroundColor(Color.WHITE);
+									tableRowToAdd.originalColor = Color.WHITE;
+								}
 								textViewToAdd.setText(trackName);
-								tableRowToAdd.addView(textViewToAdd);
+								textTwoViewToAdd.setText(trackArtist);
+								textViewToAdd.setTextSize(20);
+								textTwoViewToAdd.setTextColor(Color.DKGRAY);
+								LinearLayout linearLayoutToAdd = new LinearLayout(SearchActivity.this);
+								linearLayoutToAdd.setOrientation(LinearLayout.VERTICAL);
+								linearLayoutToAdd.addView(textViewToAdd);
+								linearLayoutToAdd.addView(textTwoViewToAdd);
+								tableRowToAdd.setOnTouchListener(rowTap);
+								tableRowToAdd.addView(linearLayoutToAdd);
 								searchLayout.addView(tableRowToAdd);
 								
 							}
---
+++ src/com/lsu/vizeq/TrackRow.java
@@ -0,0 +1,40 @@
+package com.lsu.vizeq;
+
+import android.content.Context;
+import android.widget.TableRow;
+
+public class TrackRow extends TableRow
+{
+
+	public String mTrack;
+	public String mAlbum;
+	public String mArtist;
+	public String mUri;
+	public int originalColor;
+	
+	public TrackRow(Context context, String track, String album, String artist, String uri) {
+		super(context);
+		mTrack = track;
+		mAlbum = album;
+		mArtist = artist;
+		mUri = uri;
+	}
+	
+	public TrackRow(Context context)
+	{
+		super(context);
+	}
+
+	public String getSpotifyUri() {
+		return mUri;
+	}
+
+	public String getTrackInfo() {
+		return mAlbum + " - " + mArtist;
+	}
+
+	public CharSequence getTrackName() {
+		return mTrack;
+	}
+
+}
