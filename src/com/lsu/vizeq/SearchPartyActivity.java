14cb4e342ad22bebe29cd82ed0a011f76a992b12
jo-nguyen <jo_nguyen92@yahoo.com>
2014-03-24 18:36:54 -0500

    Send Button added
    Button added to profile tab in settings for sending track info (no code
    implemented yet for it) and other menu fixes.

--- src/com/lsu/vizeq/SearchPartyActivity.java
+++ src/com/lsu/vizeq/SearchPartyActivity.java
@@ -321,18 +321,11 @@ public class SearchPartyActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
+		case R.id.action_settings:
+			Intent nextIntent  = new Intent(SearchPartyActivity.this, ProfileActivity.class);
+			startActivity(nextIntent);
 		}
-		return super.onOptionsItemSelected(item);
+		return true;
 	}
 
 }
