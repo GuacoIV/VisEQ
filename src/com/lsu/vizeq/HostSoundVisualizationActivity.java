67fe537bb6c81a25c42293109c90d3bab1565baf
Adam Fitzgerald <afitzg6@lsu.edu>
2014-04-22 12:23:31 -0500

    visualizer much better, now need to make it send to partygoers

--- src/com/lsu/vizeq/HostSoundVisualizationActivity.java
+++ src/com/lsu/vizeq/HostSoundVisualizationActivity.java
@@ -1,14 +1,21 @@
 package com.lsu.vizeq;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class HostSoundVisualizationActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_host_sound_visualization);
 		
 		VisualizerView vizView = (VisualizerView)findViewById(R.id.visualizer);
--- src/com/lsu/vizeq/VisualizerView.java
+++ src/com/lsu/vizeq/VisualizerView.java
@@ -4,65 +4,89 @@ import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.media.audiofx.Visualizer;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
-import android.media.audiofx.Visualizer;
 
-class Circle {
 
-}
 
 public class VisualizerView extends View {
 
-
-
-	private byte[] fromBytes = new byte[50000];
-	private byte[] toBytes = new byte[50000];
-	private Rect mRect = new Rect();
+	private class FrequencyCircle {
+		private float prevEnergy;
+		private float energy;
+		private Paint mPaint;
+		private ObjectAnimator mAnimator;
+		
+		private float size;
+		private float onSize;
+		private float offSize;
+		private float threshold = 1.1f;
+		
+		public FrequencyCircle(Paint p, float on, float off) {
+			mPaint = p;
+			onSize = on;
+			offSize = off;
+			mAnimator = new ObjectAnimator();
+			mAnimator.setTarget(this);
+			mAnimator.setInterpolator(mInterpolator);
+			mAnimator.setDuration(captureRate/10);
+		}
+		
+		public void setSize(float s) {
+			size = s;
+			invalidate();
+		}
+		
+		public void SetEnergy(float s) {
+			boolean isOnLastFrame = false;
+			boolean isOnThisFrame = false;
+			if (energy > threshold*prevEnergy) {
+				isOnLastFrame = true;
+			}
+			prevEnergy = energy;
+			energy = s;
+			if (energy > threshold*prevEnergy) {
+				isOnThisFrame = true;
+			}
+			if (isOnLastFrame ^ isOnThisFrame) {
+				if (isOnThisFrame) {
+					mAnimator.setValues(PropertyValuesHolder.ofFloat("size", size, onSize));
+				}
+				else {
+					mAnimator.setValues(PropertyValuesHolder.ofFloat("size", size, offSize));
+				}
+				mAnimator.start();
+			}
+		}
+		
+		public void render(Canvas canvas) {
+			Log.d("color", String.valueOf(mPaint.getColor()));
+			canvas.drawCircle(getWidth()/2, getHeight()/2, size*getWidth(), mPaint);
+		}
+	}
+	
+	private final int NUM_BANDS = 4;
+	
 	private Visualizer mVisualizer;
-	private Paint mPaint;
-
-	private float[] pastEnergies;
-
-	private ObjectAnimator mAnimator;
-
-	private float toSize;
-	private float size;
-	private float normalizingFactor;
-
-	private Interpolator mInterpolator = new AccelerateInterpolator();
-
+	private Interpolator mInterpolator = new DecelerateInterpolator();
 	private int captureRate;
-
-	private final float MIN_CIRCLE_FRACTION = .05f;
-	private final float MAX_CIRCLE_FRACTION = 1f;
-	private final float DIFF_CIRCLE_FRACTION = MAX_CIRCLE_FRACTION - MIN_CIRCLE_FRACTION;
-
+	private FrequencyCircle[] circles = new FrequencyCircle[NUM_BANDS];
+	
 	public VisualizerView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
-	public void setSize(float s) {
-		size = s;
-		if (size < 0) size = 0;
-		if (size > 1) size = 1;
-		invalidate();
-	}
-
-	private int actualSize() {
-		normalizingFactor = getWidth()/2;
-		float t = (MIN_CIRCLE_FRACTION + DIFF_CIRCLE_FRACTION*size)*normalizingFactor;
-		Log.d(String.valueOf(t), "ttt");
-		return (int)(t);
-	}
-
 	public void init(int audioSessionId) {
 		mVisualizer = new Visualizer(audioSessionId);
+		if (mVisualizer.getEnabled()) {
+			mVisualizer.setEnabled(false);
+		}
 		mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[0]);
 
 		captureRate = Visualizer.getMaxCaptureRate()/4;
@@ -72,69 +96,43 @@ public class VisualizerView extends View {
 			@Override
 			public void onWaveFormDataCapture(Visualizer arg0, byte[] arg1, int arg2) {
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void onFftDataCapture(Visualizer arg0, byte[] arg1, int arg2) {
-				//System.arraycopy(arg1, 0, fromBytes, 0, arg1.length);
-				int numSamples = arg1.length/4;
-
-				double maxSize = numSamples*16;
-				toSize = 0;
-				for (int i = 0; i < numSamples; i++) {
-					toSize += Math.abs(arg1[i]);
+				int bandWidth = arg1.length/NUM_BANDS;
+				for (int j = 0; j < NUM_BANDS; j++) {
+					float sum = 0;
+					for (int i = bandWidth*j; i < bandWidth*(j+1); i++) {
+						sum += Math.abs(arg1[i]);
+					}
+					sum /= bandWidth;
+					circles[j].SetEnergy(sum);
 				}
-				toSize /= maxSize;
-
-				float avg = 0;
-				for (int i = 0; i < pastEnergies.length; i++) {
-					avg += pastEnergies[i];
-				}
-				avg /= pastEnergies.length;
-
-				float c = 1.2f;
-
-				if (toSize <= c*avg) {
-					toSize = 0;
-				}
-
-				mAnimator.setValues(PropertyValuesHolder.ofFloat("size", size, toSize));
-				mAnimator.start();
 			}
 		};
 
 		mVisualizer.setDataCaptureListener(captureListener, captureRate, false, true);
 		mVisualizer.setEnabled(true);
-
-		mPaint = new Paint();
-
-		mAnimator = new ObjectAnimator();
-		mAnimator.setTarget(this);
-		mAnimator.setInterpolator(mInterpolator);
-		mAnimator.setDuration(captureRate/10);
-
-		Log.d(String.valueOf(captureRate), "captureRate");
-
-		pastEnergies = new float[captureRate];
-
-		mPaint.setColor(VizEQ.colorScheme);
-
+		
+		for (int i = 0; i < circles.length; i++) {
+			Paint p = new Paint();
+			float[] hsv = new float[3];
+			Color.colorToHSV(VizEQ.colorScheme, hsv);
+			hsv[2] = 1.0f - (0.8f - i * 0.6f) * (1.0f - hsv[2]);
+			int lightenedColor = Color.HSVToColor(hsv);
+			p.setColor(lightenedColor);
+			p.setAlpha(255/4);
+			circles[circles.length - 1 - i] = new FrequencyCircle(p, (.5f - i * .05f), .1f);
+		}
 	}
 
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-
-		int w = getWidth();
-		int h = getHeight();
-
-		canvas.drawCircle(w/2, h/2, actualSize(), mPaint);
-
+		this.setBackgroundColor(Color.BLACK);
+		for (int i = 0; i < circles.length; i++) {
+			circles[i].render(canvas);
+		}
 	}
-
-
-
-	
-	
 
 }
