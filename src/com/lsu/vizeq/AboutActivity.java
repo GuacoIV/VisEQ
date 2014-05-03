9ddf865fa33c114d7761733a780e8a55323bf309
jo-nguyen <jo_nguyen92@yahoo.com>
2014-05-03 13:40:02 -0500

    Small fixes
    Titles changed to VIZEQ for all screens
    About now follows color changes

--- src/com/lsu/vizeq/AboutActivity.java
+++ src/com/lsu/vizeq/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.lsu.vizeq;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Menu;
 
 public class AboutActivity extends BackableActivity
@@ -12,6 +15,30 @@ public class AboutActivity extends BackableActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
+		
+		SharedPreferences memory = getSharedPreferences("VizEQ",MODE_PRIVATE);
+		ActionBar actionBar = getActionBar();
+		int posi = memory.getInt("colorPos", -1);
+		if (posi > 0) VizEQ.numRand = posi;	
+		
+		switch (VizEQ.numRand)
+		{
+			case 1:
+				actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.Red)));
+				break;
+			case 2:
+				actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.Green)));				
+				break;
+			case 3:
+				actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.Blue)));
+				break;
+			case 4:
+				actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.Purple)));				
+				break;
+			case 5:
+				actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.Orange)));
+				break;					
+		}
 	}
 
 	@Override
