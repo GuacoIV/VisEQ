d62cc1b4ad813bdc3e65761026f5920b437be0fe
treeffle <treeffle@cox.net>
2014-05-06 15:18:53 -0500

    Trying to not crash when network issues happen

--- src/com/lsu/vizeq/HostActivity.java
+++ src/com/lsu/vizeq/HostActivity.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 import java.util.List;
 import java.util.Locale;
 
@@ -15,7 +16,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import android.app.ActionBar;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -384,12 +384,13 @@ public class HostActivity extends BackableActivity
 			zipcode = params[0];
 			ip = getPrivateIp();
 			
-			
-			Jedis jedis = myapp.jedisPool.getResource();
-			try{
+			Jedis jedis = null;
+			try
+			{
+				jedis = myapp.jedisPool.getResource();
 				jedis.auth(Redis.auth);
-				
-				
+
+
 				long reply = jedis.setnx(zipcode + ":" + partyName, ip);
 				if(reply == 0)
 					result = 1;
@@ -413,7 +414,6 @@ public class HostActivity extends BackableActivity
 				if(jedis != null)
 					myapp.jedisPool.returnResource(jedis);
 			}
-			
 			return result;
 		}
 
