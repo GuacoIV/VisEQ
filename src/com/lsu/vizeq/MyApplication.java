ae0d534ef1637667e4bd8f667d1c08dd9bc7852d
treeffle <treeffle@cox.net>
2014-05-07 16:29:53 -0500

    warning for nexus 7 users

--- src/com/lsu/vizeq/MyApplication.java
+++ src/com/lsu/vizeq/MyApplication.java
@@ -40,11 +40,8 @@ public class MyApplication extends Application {
 		 * Workaround attempt for Nexus 7.  See what the brand and model strings actually are in debug mode and adjust accordingly.
 		 * Then, it would solo that stream for the whole application, but I think our sound driver probably won't push the music out through there.
 		 * Give it a try though.
-		if ((brand + model).equals("Nexus 7")) 
-		{
-			AudioManager manager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-			manager.setStreamSolo(AudioManager.STREAM_ALARM, true);
-		}*/
+		*/
+		
 	}
 	
 	@Override
